29192da205bc9eecb860b4072d350b8c6cb90d45
Vindya3110 <vindyavasini3110@gmail.com>
2025-05-24 19:01:45 +0530

    Refactor App component and update build configuration

---
+++ src/webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../dist'),
+            serveIndex: false,
+        },
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+    },
+    output: {
+        publicPath: 'http://localhost:3000/',
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        overrides: [
+                            {
+                                test: /\.(js|jsx)$/i,
+                                compact: false,
+                            },
+                        ],
+                    },
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ['@svgr/webpack'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new ModuleFederationPlugin({
+            name: 'onehub',
+            remotes: {
+                remote1: 'subscription@http://localhost:3001/remoteEntry.js',
+                remote2: 'ownership@http://localhost:3002/remoteEntry.js',
+            },
+            shared: {
+                react: {
+                    singleton: true,
+                    requiredVersion: '^19.1.0',
+                    eager: true,
+                },
+                'react-dom': {
+                    singleton: true,
+                    requiredVersion: '^19.1.0',
+                    eager: true,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, '../src/'),
+        },
+    },
+};
